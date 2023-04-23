ab6e7e0da26eb5b4f5bc0808b5efec3a4cdd64c7
Ruan Failache <ruanfailache@gmail.com>
2023-04-23 13:51:26 -0300

    remove unused methods at session repository

--- src/features/session/session.repository.ts
+++ src/features/session/session.repository.ts
@@ -6,22 +6,6 @@ import { PrismaProvider } from '@adapters/prisma/prisma.provider';
 export class SessionRepository {
 	constructor(private readonly prismaProvider: PrismaProvider) {}
 
-	private async findByUserId(userId: string) {
-		return this.prismaProvider.session.findFirst({
-			where: {
-				userId,
-			},
-		});
-	}
-
-	private async delete(sessionId: string) {
-		await this.prismaProvider.session.delete({
-			where: {
-				id: sessionId,
-			},
-		});
-	}
-
 	async create(userId: string, accessToken: string) {
 		await this.prismaProvider.session.create({
 			data: {
